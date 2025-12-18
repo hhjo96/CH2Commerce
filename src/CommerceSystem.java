438197314a64e6b3592bc2ca2dc04b79567c098e
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-18 15:06:52 +0900

    ADD: Step 2

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class CommerceSystem {
+
+    Scanner sc = new Scanner(System.in);
+        private List<Product> productList= new ArrayList<Product>();
+  //  private List<Category> categories
+     //   private List<Customer> customers
+    //    private List<Order> orders
+    //    private List<ShoppingCart> shoppingCarts
+
+    public CommerceSystem(List<Product> productList) {
+        this.productList = productList;
+    }
+
+        public void start() {
+            while(true){
+                Printer.printStart();
+                Printer.printList(productList);
+                Printer.printZero();
+                if(sc.nextInt() == 0) break;
+            }
+            Printer.printEnd();
+        }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
         //Product(String name, int price, String category, String description, int status, int stock) {
         Product GalaxyZflip7 = new Product("Galaxy Zflip 7", 1485000, "Electronics", "갤럭시 Z플립 7", 1, 10);
         Product iPadPro13 = new Product("iPad Pro 13", 2099000, "Electronics", "아이패드 프로 13세대", 1, 10);
         Product iPhone17 = new Product("iPhone 17", 1290000, "Electronics", "아이폰 17", 1, 10);
-        Product GalaxyBuds = new Product("Galaxy Buds 3", 169000,  "Electronics", "갤럭시 버즈 3", 1, 10);
+        Product GalaxyBuds = new Product("Galaxy Buds 3", 169000, "Electronics", "갤럭시 버즈 3", 1, 10);
         Product MacBookAir13 = new Product("MacBook Air 13", 1590000, "Electronics", "맥북 에어 13인치", 1, 10);
 
         List<Product> productList = new ArrayList<>();
@@ -23,14 +20,8 @@ public class Main {
         productList.add(GalaxyBuds);
         productList.add(MacBookAir13);
 
-        while(true){
-            Printer.printStart();
-            Printer.printList(productList);
-            Printer.printZero();
-            if(sc.nextInt() == 0) break;
-        }
-        Printer.printEnd();
+        CommerceSystem cs = new CommerceSystem(productList);
+
+        cs.start();
     }
-
-
 }
